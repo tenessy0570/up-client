c86cbf228aa17d18c6ecca65d513b6c26d8b4ad5
Плотников Роман Вячеславович <gr403_plrvya@ttit.local>
2022-03-14 17:17:24 +0700

    added core classes and bootstrap.php

---
+++ core/bootstrap.php
@@ -0,0 +1,28 @@
+<?php
+
+const DIR_CONFIG = '/../config';
+
+spl_autoload_register(function ($className) {
+    $paths = include __DIR__ . DIR_CONFIG . '/path.php';
+    $className = str_replace('\\', '/', $className);
+
+    foreach ($paths['classes'] as $path) {
+        $filename = $_SERVER['DOCUMENT_ROOT'] . "/$paths[root]/$path/$className.php";
+        if (file_exists($filename)) {
+            require_once $filename;
+        }
+    }
+});
+
+function getConfigs(string $path = DIR_CONFIG): array {
+    $settings = [];
+    foreach (scandir(__DIR__ . $path) as $file) {
+        $name = explode('.', $file)[0];
+        if (!empty($name)) {
+            $settings[$name] = include __DIR__ . "$path/$file";
+        }
+    }
+    return $settings;
+}
+
+return new Src\Application(new Src\Settings(getConfigs()));
--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo "Hello";
+declare(strict_types=1);
+
+try {
+    $app = require_once __DIR__ . '/../core/bootstrap.php';
+    $app->run();
+} catch (\Throwable $exception) {
+    echo "<pre>";
+    print_r($exception);
+    echo "</pre>";
+}
