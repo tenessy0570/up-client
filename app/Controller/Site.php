a0c0aaef6fd1f23fa96f3b1a985ded896efb52f2
Плотников Роман Вячеславович <gr403_plrvya@ttit.local>
2022-03-15 16:48:05 +0700

    using illuminate Models

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -4,12 +4,13 @@ namespace Controller;
 
 use Src\View;
 use Illuminate\Database\Capsule\Manager as DB;
+use Model\Post;
 
 class Site
 {
     public function index(): string
     {
-        $posts = DB::table('posts')->get();
+        $posts = Post::all();
         return (new View())->render('site.post', ['posts' => $posts]);
     }
 
