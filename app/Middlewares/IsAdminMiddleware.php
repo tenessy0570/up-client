d3a4974937ec28aaa0a1248d6ef038e333ee35bd
Плотников Роман Вячеславович <gr403_plrvya@ttit.local>
2022-03-17 18:56:39 +0700

    todo: isAdmin middleware. grouping urls to know if url is for admins

--- app/Middlewares/IsAdminMiddleware.php
+++ app/Middlewares/IsAdminMiddleware.php
@@ -5,12 +5,19 @@ namespace Middlewares;
 use Src\Auth\Auth;
 use Src\Request;
 
-class AuthMiddleware
+class IsAdminMiddleware
 {
     public function handle(Request $request)
     {
         $isAdmin = Auth::isAdmin();
-        $urlsForAdmin = [];
+        $urlsForAdmin = [
+            '/createNewUser',
+            '/createNewState',
+            '/createNewDivision',
+            '/deleteUser',
+            '/deleteState',
+            '/deleteDivision'
+        ];
         $url = $_SERVER['REQUEST_URI'];
 
         if (in_array($url, $urlsForAdmin)) {
