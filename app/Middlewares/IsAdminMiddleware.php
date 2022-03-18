392c823a4dd4f3b0bdc82195911d1aa500330bc5
Плотников Роман Вячеславович <gr403_plrvya@ttit.local>
2022-03-18 16:37:32 +0700

    refactor

--- app/Middlewares/IsAdminMiddleware.php
+++ app/Middlewares/IsAdminMiddleware.php
@@ -4,29 +4,15 @@ namespace Middlewares;
 
 use Src\Auth\Auth;
 use Src\Request;
-use Src\Settings;
 
 class IsAdminMiddleware
 {
     public function handle(Request $request)
     {
-        // Перенаправляет на главную страницу, если не админ пытается выполнить админское действие
         $isAdmin = Auth::isAdmin();
-        $url = $request->url;
-
-        $allUris = Settings::getUris();
-        $urlsForAdmin = [];
-
-        foreach ($allUris as $uri) {
-            $explodedUri = explode('.', $uri);
-            if ($explodedUri[0] === 'admin') array_push($urlsForAdmin, $explodedUri[1]);
-        }
-
-
-        if (in_array($url, $urlsForAdmin)) {
-            if (!$isAdmin) {
-                app()->route->redirect('/hello');
-            }
+        // Перенаправляет на главную страницу, если не админ пытается выполнить админское действие
+        if (!$isAdmin) {
+            app()->route->redirect('/hello');
         }
     }
 }
